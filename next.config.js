e6c95bb0f4284850ea7bebc984da69d9aae9654f
Andrei <webconsult.ekb@gmail.com>
2022-03-13 21:16:40 +0100

    webpack configured

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if(dev) {
+      console.log('Is development flow ', process.env.SECRET) // this line will fire twice because webpack function is runs for server and client separately
+    }
+    return config // do not forget to return this object
+  },
+  env: {
+    SECRET: process.env.SECRET
+  }
 }
 
 module.exports = nextConfig
